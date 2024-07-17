447e01e497331dbe15c0b58f3938397f8b085a81
Naiba Sree P <112084996+NaibaSree@users.noreply.github.com>
2024-07-17 14:56:41 +0530

    Create Delete without head pointer.java

---
+++ Delete without head pointer.java
@@ -0,0 +1,13 @@
+class Solution
+{
+    void deleteNode(Node del_node)
+    {
+         // Your code here
+         if(del_node.next==null)
+         del_node=null;
+         if(del_node.next!=null){
+         del_node.data=del_node.next.data;
+         del_node.next=del_node.next.next;
+         }
+    }
+}
